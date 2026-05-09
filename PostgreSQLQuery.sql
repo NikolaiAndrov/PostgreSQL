b02405148f25a42c4de503d36a19c93a3c3b11bf
nikolaiandrov <nikolaiandrov@abv.bg>
2026-05-09 15:53:25 +0300

    Create table person with constraints

--- PostgreSQLQuery.sql
+++ PostgreSQLQuery.sql
@@ -9,3 +9,13 @@ CREATE TABLE person (
 
 -- Drop/Delete table person
 DROP TABLE person;
+
+-- Create table person with constraints
+CREATE TABLE person (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	gender VARCHAR(7) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	email VARCHAR(150)
+	);
